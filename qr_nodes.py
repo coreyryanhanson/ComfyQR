fd35bb8e62a7835fa31f20f57a96a2151784ca2f
Corey Ryan Hanson <2010074-coreyryanhanson@users.noreply.gitlab.com>
2023-08-14 02:36:20 +0000

    Ability to seperate function patterns

--- qr_nodes.py
+++ qr_nodes.py
@@ -7,6 +7,8 @@ import torch
 class QRBase:
     def __init__(self):
         self.text = ""
+        self.fill = None
+        self.back = None
 
     FUNCTION = "generate_qr"
     CATEGORY = "Comfy-QR"
@@ -25,10 +27,10 @@ class QRBase:
         return torch.from_numpy(out_image).unsqueeze(0)
 
     def _make_qr(self, qr, fill_hexcolor, back_hexcolor):
-        fill_color = self._parse_hexcolor_string(fill_hexcolor, "fill_hexcolor")
-        back_color = self._parse_hexcolor_string(back_hexcolor, "back_hexcolor")
+        self.fill = self._parse_hexcolor_string(fill_hexcolor, "fill_hexcolor")
+        self.back = self._parse_hexcolor_string(back_hexcolor, "back_hexcolor")
         qr.make(fit=True)
-        return qr.make_image(fill_color=fill_color, back_color=back_color)
+        return qr.make_image(fill_color=self.fill, back_color=self.back)
 
     def _parse_hexcolor_string(self, s, parameter):
         if s.startswith("#"):
@@ -171,13 +173,94 @@ class QRByModuleSize(QRBase):
         return (self._img_to_tensor(img), qr.version, img.pixel_size)
 
 
+class QRByModuleSizeSplitFunctionPatterns(QRBase):
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "protocol": (["Http", "Https", "None"], {"default": "Https"}),
+                "text": ("STRING", {"multiline": True}),
+                "module_size": ("INT", {"default": 16, "min": 1, "max": 64, "step": 1}),
+                "max_image_size": ("INT", {"default": 512, "min": 64, "max": 4096, "step": 64}),
+                "fill_hexcolor": ("STRING", {"multiline": False, "default": "#000000"}),
+                "back_hexcolor": ("STRING", {"multiline": False, "default": "#FFFFFF"}),
+                "error_correction": (["Low", "Medium", "Quartile", "High"], {"default": "High"}),
+                "border": ("INT", {"default": 1, "min": 0, "max": 100, "step": 1}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE", "IMAGE", "IMAGE", "MASK", "INT", "INT")
+    RETURN_NAMES = ("QR_FLATTENED", "MODULE_LAYER", "FINDER_LAYER", "FINDER_MASK", "QR_VERSION", "IMAGE_SIZE")
+
+    def _generate_finder_pattern_ranges(self, module_size, border_size):
+        outer = module_size * border_size
+        inner = 7 * module_size + outer
+        return [
+            (outer, inner, outer, inner),
+            (outer, inner, -inner, -outer),
+            (-inner, -outer, outer, inner)
+        ]
+
+    def _generate_finder_pattern_mask(self, pixel_size, module_size, border_size):
+        mask = np.zeros((pixel_size, pixel_size), dtype=bool)
+        for x_min, x_max, y_min, y_max in self._generate_finder_pattern_ranges(module_size, border_size):
+            mask[y_min:y_max, x_min:x_max] = True
+        return mask
+
+    def _apply_fill_to_mask(self, img, mask):
+        array = np.array(img).copy()
+        indices = np.nonzero(mask)
+        array[indices[0], indices[1], :] = self.back
+        return Image.fromarray(array)
+
+    def _mask_to_tensor(self, mask):
+        out_image = mask.astype(np.float32)
+        return torch.from_numpy(out_image)
+
+    def generate_qr(
+            self,
+            protocol,
+            text,
+            module_size,
+            max_image_size,
+            fill_hexcolor,
+            back_hexcolor,
+            error_correction,
+            border
+            ):
+        self.update_text(protocol, text)
+        error_level = self._get_error_correction_constant(error_correction)
+        qr = qrcode.QRCode(
+                error_correction=error_level,
+                box_size=module_size,
+                border=border)
+        qr.add_data(self.text)
+        img = self._make_qr(qr, fill_hexcolor, back_hexcolor)
+        pixel_size = img.pixel_size
+        self._validate_qr_size(pixel_size, max_image_size)
+        mask = self._generate_finder_pattern_mask(pixel_size, module_size, border)
+        module_image = self._apply_fill_to_mask(img, mask)
+        function_image = self._apply_fill_to_mask(img, ~mask)
+        return (
+            self._img_to_tensor(img),
+            self._img_to_tensor(module_image),
+            self._img_to_tensor(function_image),
+            self._mask_to_tensor(mask),
+            qr.version,
+            pixel_size,
+            )
+
+
 NODE_CLASS_MAPPINGS = {
                        "comfy-qr-by-module-size": QRByModuleSize,
                        "comfy-qr-by-image-size": QRByImageSize,
+                       "comfy-qr-by-module-split": QRByModuleSizeSplitFunctionPatterns,
                        }
 
 
 NODE_DISPLAY_NAME_MAPPINGS = {
                               "comfy-qr-by-module-size": "QR Code",
                               "comfy-qr-by-image-size": "QR Code (Conformed to Image Size)",
+                              "comfy-qr-by-module-split": "QR Code (Split)",
                               }
